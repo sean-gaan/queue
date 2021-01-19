803e0219ea092575653fb4209f4d4f8e0ebd50ae
Sean Jewell <sean@gaan.dev>
2021-01-19 16:17:45 +0000

    bring in blade based frontend, tailwind css, and fix auth to go through standard laravel procedure

---
+++ app/Http/Controllers/Web/Auth/LogoutController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Web\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class LogoutController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    public function __construct()
+    {
+        $this->middleware(['web']);
+    }
+
+    public function store(Request $request)
+    {
+        Auth::guard('web')->logout();
+        return success([]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::get('login', [\App\Http\Controllers\Web\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [\App\Http\Controllers\Web\Auth\LoginController::class, 'login']);
+Route::post('logout', [\App\Http\Controllers\Web\Auth\LoginController::class, 'logout'])->name('logout');
+
+// Registration Routes...
+Route::get('register', [\App\Http\Controllers\Web\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [\App\Http\Controllers\Web\Auth\RegisterController::class, 'register']);
+
+// Password Reset Routes...
+Route::get('password/reset', [\App\Http\Controllers\Web\Auth\ForgotPasswordController::class, 'showLinkRequestForm']);
+Route::post('password/email', [\App\Http\Controllers\Web\Auth\ForgotPasswordController::class, 'sendResetLinkEmail']);
+Route::get('password/reset/{token}', [\App\Http\Controllers\Web\Auth\ResetPasswordController::class, 'showResetForm']);
+Route::post('password/reset', [\App\Http\Controllers\Web\Auth\ResetPasswordController::class, 'reset']);
+
+Route::get('/', [\App\Http\Controllers\Web\IndexController::class, 'index'])->name('index');
+Route::get('/home', [\App\Http\Controllers\Web\HomeController::class, 'index'])->name('home');
+
